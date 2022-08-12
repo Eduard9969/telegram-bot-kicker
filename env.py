c3d3ff01365ffcb05df619dafa0c7f9dbe052b12
Eduard9969 <eduard9969@gmail.com>
2022-08-12 18:28:10 +0300

    Remove unused env variables

--- env.py
+++ env.py
@@ -8,6 +8,3 @@ IS_DEBUG = os.getenv('IS_DEBUG', default=False)
 
 TELEGRAM_BOT_AUTH_TOKEN = os.getenv('TELEGRAM_BOT_AUTH_TOKEN', default=None)
 TELEGRAM_INSPECTOR_CHANNEL = os.getenv('TELEGRAM_INSPECTOR_CHANNEL', default=None)
-
-TELEGRAM_APP_CLIENT_ID = os.getenv('TELEGRAM_BOT_APP_CLIENT_ID', default=None)
-TELEGRAM_APP_API_HASH = os.getenv('TELEGRAM_BOT_APP_API_HASH', default=None)
